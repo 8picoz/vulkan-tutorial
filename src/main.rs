e366c37e1dcac47582b297a5e02a8ed5e43c4eb2
tata <8picoz@tata.pw>
2022-01-31 17:58:35 +0900

    [chore] env_logger and log crate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::{error::Error, result::Result, fmt::Result};
+
+struct VulkanApp;
+
+impl VulkanApp {
+    fn new() -> Result<Self, Box<dyn Error>> {
+        
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+
 }
